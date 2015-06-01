406fa7610f4e67f5959803a48bdc835a8b97b496
Gerard Stanczak <gerard.stanczak@proofhq.com>
2015-06-01 22:11:18 +0200

    Add S.M.A.R.T monitoring

---
+++ bin/hdd_discovery.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Script for HDD discovery
+# Created by:
+# Michał Macioszek, Taras Baran, Michal Gębora, Marcin Wilk, Maks Bednarek, Anna Fałek, Mikołaj Szczuraszek
+#
+
+
+disks=`ls -l /dev/sd* | awk '{print $NF}' | sed 's/[0-9]//g' | uniq`
+echo -n "{\"data\":["
+
+comma=1
+for disk in $disks
+do
+    if [ $comma -eq 0 ]; then
+        echo -n ","
+    fi
+    comma=0
+    echo -n "{\"{#DISKNAME}\":\"$disk\",\"{#SHORTDISKNAME}\":\"${disk:5}\"}"
+done
+
+echo -n "]}"
