b4dc2318932805cdf8bcec8dbf9f6bd192fb11ee
Gerard Stanczak <gerard.stanczak@proofhq.com>
2016-07-07 20:28:16 +0200

    updted mysql scripts to zabbix 3.0 and newest ss_get_mysql_stats

--- bin/mysql_print_table_size.sh
+++ bin/mysql_print_table_size.sh
@@ -12,12 +12,18 @@ if [ $# -ne 2 ]; then
   exit 1
 fi
 
+if [ -e /san/mysql-fs/mysql ]; then
+  path="/san/mysql-fs/mysql/"
+else
+  path="/var/lib/mysql/"
+fi
+
 cachefileage=$(($(date +%s) - $(stat -c %Y $cachefile)))
 process_running=$(ps aux | grep 'find /var/lib/mysql' | grep -v "grep" | wc -l)
 
 if [ "$cachefileage" -gt 60 ] && [ "$process_running" -eq 0 ]; then
 
-  sudo /bin/find /var/lib/mysql -type f -printf %p+%s= | tr "=" "\n" | grep -v "\/mysql\/mysql\/" | grep "\.ibd" | sed 's/@002d/\-/g' > $cachefile.$random
+  sudo /bin/find $path -type f -printf %p+%s= | tr "=" "\n" | grep -v "\/mysql\/mysql\/" | grep "\.ibd" | sed 's/@002d/\-/g' > $cachefile.$random
 
         if [ $? -eq 0 ]; then
                 mv $cachefile.$random $cachefile
